db41cba8baedf5823693b06e24537f933b773411
Harry Chen <harrychenca@gmail.com>
2022-11-01 09:48:16 +0000

    Fix tests

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,6 +3,7 @@ class TasksController < ApplicationController
     @tasks = Task.all
     render 'tasks/index' # can be omitted
   end
+
   def create
     @task = Task.new(task_params)
     if @task.save
@@ -12,30 +13,26 @@ class TasksController < ApplicationController
 
   def destroy
     @task = Task.find_by(id: params[:id])
-    if @task and @task.destroy
+    if @task&.destroy
       render json: { success: true }
     else
       render json: { success: false }
     end
   end
-  
+
   def mark_complete
     @task = Task.find_by(id: params[:id])
-    if @task and @task.update(completed: true)
-      render 'tasks/mark_complete'
-    end
+    render 'tasks/update' if @task&.update(completed: true)
   end
 
   def mark_active
     @task = Task.find_by(id: params[:id])
-    if @task and @task.update(completed: false)
-      render 'tasks/update'
-    end
+    render 'tasks/update' if @task&.update(completed: false)
   end
 
   private
 
-    def task_params
-      params.require(:task).permit(:content)
-    end
+  def task_params
+    params.require(:task).permit(:content)
+  end
 end
